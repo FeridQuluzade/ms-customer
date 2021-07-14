0e3fabfe52dadf77263d06a24226f21a191ce2f9
FeridQuluzade <feridquluzade2002@gmail.com>
2021-07-14 23:45:43 +0400

    Task-5: chance CustomerServiceImpl structure

--- src/main/java/az/bank/mscustomer/repository/CustomerRepository.java
+++ src/main/java/az/bank/mscustomer/repository/CustomerRepository.java
@@ -3,6 +3,6 @@ package az.bank.mscustomer.repository;
 import az.bank.mscustomer.repository.entity.CustomerEntity;
 import org.springframework.data.repository.CrudRepository;
 
-public interface CustomerRepository extends CrudRepository <CustomerEntity,Long>{
+public interface CustomerRepository extends CrudRepository<CustomerEntity, Long> {
 
 }
